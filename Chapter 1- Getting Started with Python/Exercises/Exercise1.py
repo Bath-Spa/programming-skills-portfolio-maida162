6d1ef65905cd16a24d8c71b5e041224d6fd66163
maida162 <maidainam162@mail.com>
2023-10-26 07:35:04 -0700

    Create Exercise1.py
    exercise1

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise1.py
@@ -0,0 +1,7 @@
+#Exercise 1: Print Strings
+print("""Twinkle, twinkle, little star,
+      How I woder what you are 
+           Up above the world so high
+           Like a diamond in the sky
+Twinkle, Twinkle, little star,
+       How I wonder what you are   """)
