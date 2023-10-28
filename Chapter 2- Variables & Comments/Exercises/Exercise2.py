e7738eca7a05e8b2a43790eba978f3deff634d4a
maida162 <maidainam162@mail.com>
2023-10-28 10:48:47 -0700

    Create Exercise2.py
    ch 2: exercise 2

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise2.py
@@ -0,0 +1,3 @@
+# Exercise 2: Variables :
+"""Find a quote from a famous person you admire. Print the quote and the name of its author."""
+print("Thomas Edison once said: Genius is one percent inspiration and ninety-nine percent perspiration.")
