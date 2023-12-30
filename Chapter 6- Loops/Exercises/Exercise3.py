bb46e48850d1bc50da1129bbde7dc96c3c3c848d
maida162 <maidainam162@mail.com>
2023-12-30 00:30:53 -0800

    Chapter6 Exercise3
    Infinity

--- Chapter 6- Loops/Exercises/Exercise3.py
+++ Chapter 6- Loops/Exercises/Exercise3.py
@@ -3,6 +3,7 @@
 """Write a loop that never ends, and run it. (To end the loop, press ctrl-C or close the window
  displaying the output.)"""
 
-# Infinite loop
 while True:
     print("This is an infinite loop. Press Ctrl-C to exit.")
+
+
