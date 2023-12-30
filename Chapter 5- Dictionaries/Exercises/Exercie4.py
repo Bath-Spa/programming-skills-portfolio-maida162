12664f297006a70a018874a7d9da56525ea079b5
maida162 <maidainam162@mail.com>
2023-12-29 23:54:40 -0800

    Chapter5 Exercise4
    rivers

--- Chapter 5- Dictionaries/Exercises/Exercie4.py
+++ Chapter 5- Dictionaries/Exercises/Exercie4.py
@@ -6,23 +6,19 @@
 * Use a loop to print the name of each river included in the dictionary.
 * Use a loop to print the name of each country included in the dictionary."""
 
-# Dictionary of major rivers and the countries they run through
 rivers = {
     'nile': 'Egypt',
     'amazon': 'Brazil',
     'yangtze': 'China'
 }
-
-# Loop to print a sentence about each river
 for river, country in rivers.items():
     print(f"The {river.capitalize()} runs through {country}.\n")
 
-# Loop to print the name of each river
 print("Rivers:")
 for river in rivers.keys():
     print(river.capitalize())
 
-# Loop to print the name of each country
 print("\nCountries:")
 for country in rivers.values():
     print(country)
+
