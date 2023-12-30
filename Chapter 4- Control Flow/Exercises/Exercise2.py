a1bc5a68332315252c2f8673a0860808ee472e8c
maida162 <maidainam162@mail.com>
2023-12-29 23:30:04 -0800

    Chapter4 Exercise2
    Alien colors

--- Chapter 4- Control Flow/Exercises/Exercise2.py
+++ Chapter 4- Control Flow/Exercises/Exercise2.py
@@ -14,3 +14,11 @@ if alien_color == 'green':
 else:
     print("Player just earned 10 points.")
 
+
+alien_color = 'green'
+
+# If-else chain
+if alien_color == 'green':
+    print("Player just earned 5 points for shooting the green alien.")
+else:
+    print("Player just earned 10 points.")
