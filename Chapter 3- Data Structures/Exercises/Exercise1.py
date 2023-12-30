127f57127f44b9d3ff27b816aac5505737ca4dec
maida162 <maidainam162@mail.com>
2023-12-30 10:15:33 -0800

    Chapter 3 Exercise1
    Names

--- Chapter 3- Data Structures/Exercises/Exercise1.py
+++ Chapter 3- Data Structures/Exercises/Exercise1.py
@@ -1,4 +1,4 @@
-# Exercise 1: Names :ballot_box_with_check:
+# Exercise 1: Names:
 
 """Store the names of a few of your friends in a list called names. Print each person’s name by accessing 
 each element in the list, one at a time."""
